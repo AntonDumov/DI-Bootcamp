33475ceeac3d492e6b8aff0121aa1ad699101c18
Anton Dumov <anton.dumov@gmail.com>
2023-12-12 19:03:05 +0200

    adding exercises texts

--- week_03/day_2/daily_callenge/scripts.js
+++ week_03/day_2/daily_callenge/scripts.js
@@ -1,26 +1,37 @@
+// Exercise 1:
+// Using this array :
+//
+// const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+//
+// Remove Banana from the array.
+// Sort the array in alphabetical order.
+// Add “Kiwi” to the end of the array.
+// Remove “Apples” from the array. Don’t use the same method as in part 1.
+// Sort the array in reverse order. (Not alphabetical, but reverse the current Array i.e. [‘a’, ‘c’, ‘b’] becomes [‘b’, ‘c’, ‘a’])
+// At the end you should see this outcome:
+// ["Kiwi", "Oranges", "Blueberries"]
+
 const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
 
-function exercise1() {
-    let newFruits = fruits
-    newFruits.shift()
-    newFruits = newFruits.sort((a, b) => {
-        return a.localeCompare(b)
-    })
-    newFruits.push('Kiwi')
-    newFruits = newFruits.filter(v => {
-        return v !== 'Apples'
-    })
-    newFruits.reverse()
-    console.log(newFruits)
-}
+let newFruits = fruits
+newFruits.shift()
+newFruits = newFruits.sort((a, b) => {
+    return a.localeCompare(b)
+})
+newFruits.push('Kiwi')
+newFruits = newFruits.filter(v => {
+    return v !== 'Apples'
+})
+newFruits.reverse()
+console.log(newFruits)
+
+
+// Exercise 2:
+// Using this array :
+// const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+//
+// Access and then console.log “Oranges”.
 
 const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
 
-function exercise2() {
-    console.log(moreFruits[1][1][0])
-}
-
-console.log('Exercise 1:')
-exercise1()
-console.log('\nExercise 2:')
-exercise2()
+console.log(moreFruits[1][1][0])
--- week_03/day_2/exercise_xp/scripts.js
+++ week_03/day_2/exercise_xp/scripts.js
@@ -1,54 +1,101 @@
-function exercise1(food='pasta', meal='dinner') {
-    console.log(`I eat ${food} at every ${meal}`);
-}
+// 🌟 Exercise 1: Your Favorite Food
+// Instructions
+// Store your favorite food into a variable.
+//
+// Store your favorite meal of the day into a variable (ie. breakfast, lunch or dinner)
+//
+// Console.log I eat <favorite food> at every <favorite meal>
 
-function exercise2() {
-    // Part i
-    const myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
-    const myWatchedSeriesLength = myWatchedSeries.length
-    const myWatchedSeriesSentence = myWatchedSeries.join(', ')
-    console.log(`I watched ${myWatchedSeriesLength} series: ${myWatchedSeriesSentence}`)
+const food = 'pasta'
+const meal = 'dinner'
+console.log(`I eat ${food} at every ${meal}`);
 
-    // Part ii
-    myWatchedSeries[2] = 'friends'
-    myWatchedSeries.push('soprano')
-    myWatchedSeries.unshift('firefly')
-    myWatchedSeries.splice(1, 1)
-    console.log(myWatchedSeries[1][2])
-    console.log(myWatchedSeries.join(', '))
-}
+// 🌟 Exercise 2 : Series
+// Instructions
+// Part I
+// Using this array : const myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+//
+// Create a variable named myWatchedSeriesLength that is equal to the number of series in the myWatchedSeries array.
+//
+// Create a variable named myWatchedSeriesSentence, that is equal to a sentence describing the series you watched
+// For example : black mirror, money heist, and the big bang theory
+//
+// Console.log a sentence using both of the variables created above
+// For example : I watched 3 series : black mirror, money heist, and the big bang theory
+//
+//
+// Part II
+// Change the series “the big bang theory” to “friends”. Hint : You will need to use the index of “the big bang theory” series.
+// Add, at the end of the array, the name of another series you watched.
+// Add, at the beginning of the array, the name of your favorite series.
+// Delete the series “black mirror”.
+// Console.log the third letter of the series “money heist”.
+// Finally, console.log the myWatchedSeries array, to see all the modifications you’ve made.
 
-function exercise3(temperatureCelsius=0) {
-    const temperatureFahrenheit = temperatureCelsius / 5 * 9 + 32
-    console.log(`${temperatureCelsius}°C is ${temperatureFahrenheit}°F.`)
-}
+// Part i
+const myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+const myWatchedSeriesLength = myWatchedSeries.length
+const myWatchedSeriesSentence = myWatchedSeries.join(', ')
+console.log(`I watched ${myWatchedSeriesLength} series: ${myWatchedSeriesSentence}`)
 
-function exercise4() {
-    let c;
-    let a = 34;
-    let b = 21;
+// Part ii
+myWatchedSeries[2] = 'friends'
+myWatchedSeries.push('soprano')
+myWatchedSeries.unshift('firefly')
+myWatchedSeries.splice(1, 1)
+console.log(myWatchedSeries[1][2])
+console.log(myWatchedSeries.join(', '))
 
-    // 1
-    console.log(a + b) //first expression
-    // Prediction: it will output 55 cause "a" and "b" are numbers and 34 + 21 equals 55
-    // Actual: 55
+// 🌟 Exercise 3 : The Temperature Converter
+// Instructions
+// Store a celsius temperature into a variable.
+//
+// Convert it to fahrenheit and console.log <temperature>°C is <temperature>°F.
+// Hint : Should you create another variable to hold the temperature in fahrenheit? (ie. point 2)
+// Hint: To convert a temperature from celsius to fahrenheit : Divide it by 5, then multiply it by 9, then add 32
 
-    a = 2;
+const temperatureCelsius = 0;
+const temperatureFahrenheit = temperatureCelsius / 5 * 9 + 32
+console.log(`${temperatureCelsius}°C is ${temperatureFahrenheit}°F.`)
 
-    // 2
-    console.log(a + b) //second expression
-    // Prediction: it will output 23 cause "a" and "b" are numbers and "a" is now 2 instead of 34
-    // Actual: 23
 
-    // 3
-    // value of "c" will be "undefined"
+// 🌟 Exercise 4 : Guess The Answers #1
+// Instructions
+// For each expression, predict what you think the output will be in a comment (//) without first running the command.
+// Of course, explain each prediction.
+// Then run the expression in the console. Note the actual output in a comment and compare it with your prediction.
 
-    // 4
-    console.log(3 + 4 + '5');
-    // Prediction: it will output 12 cause "3 + 4" will result a number 7 and concatenate string to number
-    // will result a sum of number and a "string number"
-    // Actual: "75" (concatenating string to a number will result a string)
-}
+let c;
+let a = 34;
+let b = 21;
+
+// 1
+console.log(a + b) //first expression
+// Prediction: it will output 55 cause "a" and "b" are numbers and 34 + 21 equals 55
+// Actual: 55
+
+a = 2;
+
+// 2
+console.log(a + b) //second expression
+// Prediction: it will output 23 cause "a" and "b" are numbers and "a" is now 2 instead of 34
+// Actual: 23
+
+// 3
+// value of "c" will be "undefined"
+
+// 4
+console.log(3 + 4 + '5');
+// Prediction: it will output 12 cause "3 + 4" will result a number 7 and concatenate string to number
+// will result a sum of number and a "string number"
+// Actual: "75" (concatenating string to a number will result a string)
+
+
+// 🌟 Exercise 5 : Guess The Answers #2
+// Instructions
+// For each expression, predict what you think the output will be in a comment (//) without first running the command.
+// Of course, explain each prediction.
+// Then run the expression in the console. Note the actual output in a comment and compare it with your prediction.
 
 function exercise5() {
     console.log(typeof (15))
@@ -118,73 +165,60 @@ function exercise5() {
 // Actual: false
 }
 
-function exercise6() {
-    console.log(5 + "34")
+// 🌟 Exercise 6 : Guess The Answers #3
+
+console.log(5 + "34")
 // Prediction: 534, because concatenating number with string will return a string
 // Actual: '534'
 
-    console.log(5 - "4")
+console.log(5 - "4")
 // Prediction: 1, because -"4" will return -4 and 5-4 is 1
 // Actual: 1
 
-    console.log(10 % 5)
+console.log(10 % 5)
 // Prediction: 0, operator "%" returns remainder of the division
 // Actual: 0
 
-    console.log(5 % 10)
+console.log(5 % 10)
 // Prediction: 5, same as above
 // Actual: 5
 
-    console.log("Java" + "Script")
+console.log("Java" + "Script")
 // Prediction: "JavaScript", concatenating script will return a new string
 // Actual: 'JavaScript'
 
-    console.log(" " + " ")
+console.log(" " + " ")
 // Prediction: "  ", same as above
 // Actual: '  '
 
-    console.log(" " + 0)
+console.log(" " + 0)
 // Prediction: " 0", number will be turned into string and this becomes string concatenation
 // Actual: ' 0'
 
-    console.log(true + true)
+console.log(true + true)
 // Prediction: 2, cause concatenating boolean turns it into number analog
 // Actual: 2
 
-    console.log(true + false)
+console.log(true + false)
 // Prediction: 1, same as above
 // Actual: 1
 
-    console.log(false + true)
+console.log(false + true)
 // Prediction: 1, same as above
 // Actual: 1
 
-    console.log(false - true)
+console.log(false - true)
 // Prediction: -1, same as above
 // Actual: -1
 
-    console.log(!true)
+console.log(!true)
 // Prediction: false, not true is false
 // Actual: false
 
-    console.log(3 - 4)
+console.log(3 - 4)
 // Prediction: -1
 // Actual: -1
 
-    console.log("Bob" - "bill")
+console.log("Bob" - "bill")
 // Prediction: NaN
 // Actual: NaN
-}
-
-console.log("Exercise 1:")
-exercise1()
-console.log("\nExercise 2:")
-exercise2()
-console.log("\nExercise 3:")
-exercise3()
-console.log("\nExercise 4:")
-exercise4()
-console.log("\nExercise 5:")
-exercise5()
-console.log("\nExercise 6:")
-exercise6()
