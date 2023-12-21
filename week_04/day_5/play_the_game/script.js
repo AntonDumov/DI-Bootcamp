09b48911b3fdb8f6862283c70cf2ed95a0802ca3
Anton Dumov <anton.dumov@gmail.com>
2023-12-21 20:34:40 +0200

    remove hints from comments

--- week_04/day_5/play_the_game/script.js
+++ week_04/day_5/play_the_game/script.js
@@ -2,7 +2,6 @@
 function playTheGame() {
 
     // In the function, start by asking the user if they would like to play the game
-    // (Hint: use the built-in confirm() function).
     const answer = confirm('Would you like to play the game?')
 
     //If the answer is false, alert “No problem, Goodbye”.
@@ -16,7 +15,7 @@ function playTheGame() {
 
     let number = null;
     // create a variable named computerNumber where the value is a random number between 0 and 10
-    // (Hint: Use the built-in Math.random() function). Make sure that the number is rounded.
+    // Make sure that the number is rounded.
     const computerNumber = Math.floor(Math.random() * 11)
 
     // If the user guessed more than 3 times, alert “out of chances” and exit the function.
@@ -39,11 +38,11 @@ function playTheGame() {
 
 function guessNumber() {
     // Ask the user to enter a number between 0 and 10
-    // (Hint: use the built-in prompt() function). You then have to check the validity of the user’s number :
+    // You then have to check the validity of the user’s number :
     const number = Number(prompt('Enter a number between 0 and 10'))
 
     if (isNaN(number)) {
-        // If the user didn’t enter a number (ie. if he entered another data type) alert “Sorry Not a number, Goodbye”.
+        // If the user didn’t enter a number alert “Sorry Not a number, Goodbye”.
         alert('Sorry Not a number, Goodbye');
     } else if (!(number >= 0 && number <= 10)) {
         // If the user didn’t enter a number between 0 and 10 alert “Sorry it’s not a good number, Goodbye”.
@@ -63,11 +62,11 @@ function compareNumbers(userNumber, computerNumber) {
         return true;
     } else if (userNumber > computerNumber) {
         // If userNumber is bigger than computerNumber, alert “Your number is bigger then the computer’s,
-        // guess again” (Hint: use the built-in prompt() function to ask the user for a new number).
+        // guess again”.
         alert('Your number is bigger then the computer’s, guess again')
     } else {
         // If userNumber is lower than computerNumber, alert “Your number is smaller than the computer’s,
-        // guess again” (Hint: use the built-in prompt() function to ask the user for a new number).
+        // guess again”.
         alert('Your number is smaller then the computer’s, guess again')
     }
     return false;
