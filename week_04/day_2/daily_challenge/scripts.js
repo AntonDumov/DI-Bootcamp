4725ff922434d50fdc9f2c8b6465e167a769d2d8
Anton Dumov <anton.dumov@gmail.com>
2023-12-23 23:53:11 +0200

    daily challenge done

---
+++ week_04/day_2/daily_challenge/scripts.js
@@ -0,0 +1,30 @@
+// Instructions
+// Prompt the user for several words (separated by commas).
+// Put the words into an array.
+// Console.log the words one per line, in a rectangular frame as seen below.
+// Check out the Hints and Requirements below.
+
+function wordsInTheStars() {
+    let wordsInput = prompt('Write several words separated by commas')
+    while (!wordsInput) {
+        wordsInput = prompt('Write at least one word')
+    }
+    const words = wordsInput.split(',')
+    for (const i in words) {
+        words[i] = words[i].trim()
+    }
+
+    const maxLength = words.map(x => {return x.length}).sort().pop() // [-1] doesn't work for some reason
+    let result = ''
+
+    result += '*'.repeat(maxLength + 4) + '\n'
+    for (const word of words) {
+        const wsLength = maxLength - word.length
+        result += `* ${word} ${' '.repeat(wsLength)}*\n`
+    }
+    result += '*'.repeat(maxLength + 4)
+
+    console.log(result)
+}
+
+wordsInTheStars()
