cf5c5a3f2880662d3d8b82488c591bec67819f7a
Anton Dumov <anton.dumov@gmail.com>
2023-12-24 19:12:30 +0200

    third exercise done

--- week_05/day_2/exercise_xp/scripts.js
+++ week_05/day_2/exercise_xp/scripts.js
@@ -116,3 +116,49 @@ form.addEventListener('submit', ev => {
         form.insertAdjacentElement('afterend', ul)
     }
 })
+
+
+// 🌟 Exercise 3 : Transform The Sentence
+// Instructions
+// Add this sentence to your HTML file then follow the steps :
+//
+//     <p><strong>Hello</strong> I hope you are enjoying <strong>this</strong> class. At the
+//         <strong>end</strong> you <strong>will</strong> be great Developers!
+//         <strong>Enjoy</strong> the <strong>JavaScript </strong> lessons</p>
+//
+// In the JS file:
+
+// Declare a global variable named allBoldItems.
+let allBoldItems;
+
+// Create a function called getBoldItems() that takes no parameter.
+// This function should collect all the bold items inside the paragraph and assign them to the allBoldItems variable.
+function getBoldItems() {
+    allBoldItems = document.querySelectorAll('p > strong')
+}
+
+// Create a function called highlight() that changes the color of all the bold text to blue.
+function highlight () {
+    allBoldItems.forEach((item) => {
+        item.style.color = 'blue'
+    })
+}
+
+// Create a function called returnItemsToDefault() that changes the color of all the bold text back to black.
+function returnItemsToDefault() {
+    allBoldItems.forEach((item) => {
+        item.style.color = 'black'
+    })
+}
+
+// Call the function highlight() on mouseover (ie. when the mouse pointer is moved onto the paragraph),
+// and the function returnItemsToDefault() on mouseout (ie. when the mouse pointer is moved out of the paragraph).
+// Look at this example
+const p = document.querySelector('p#exercise3')
+p.addEventListener('mouseover', ev => {
+    getBoldItems()
+    highlight()
+})
+p.addEventListener('mouseout', ev => {
+    returnItemsToDefault()
+})
