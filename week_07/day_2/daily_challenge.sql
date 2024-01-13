49b5ffdd1685f4fe4f4efe0148d034cadb16ce81
Anton Dumov <anton.dumov@gmail.com>
2024-01-13 18:06:29 +0200

    week 7 day 2 daily challenge done

--- week_07/day_2/daily_challenge.sql
+++ week_07/day_2/daily_challenge.sql
@@ -37,24 +37,27 @@ FROM SecondTab;
 
 --  Questions
 --  Q1. What will be the OUTPUT of the following statement?
+--  A1. 0
 
 SELECT COUNT(*)
 FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NULL );
 
 
 --  Q2. What will be the OUTPUT of the following statement?
+--  A2. 3, but it returned 2
 
 SELECT COUNT(*)
 FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id = 5 );
 
-
 --  Q3. What will be the OUTPUT of the following statement?
+--  A3. 0
 
 SELECT COUNT(*)
 FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab );
 
 
 --  Q4. What will be the OUTPUT of the following statement?
+--  A4. 2
 
 SELECT COUNT(*)
 FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NOT NULL );
