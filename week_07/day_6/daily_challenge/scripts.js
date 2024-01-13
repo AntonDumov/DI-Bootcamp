d303660765313e42b6226736771c42cb0c19fd82
Anton Dumov <anton.dumov@gmail.com>
2024-01-13 20:58:31 +0200

    week 7 day 6 daily challenge done

---
+++ week_07/day_6/daily_challenge/scripts.js
@@ -0,0 +1,55 @@
+// Instructions
+// Using this array:
+const gameInfo = [
+    {
+        username: "john",
+        team: "red",
+        score: 5,
+        items: ["ball", "book", "pen"]
+    },
+    {
+        username: "becky",
+        team: "blue",
+        score: 10,
+        items: ["tape", "backpack", "pen"]
+    },
+    {
+        username: "susy",
+        team: "red",
+        score: 55,
+        items: ["ball", "eraser", "pen"]
+    },
+    {
+        username: "tyson",
+        team: "green",
+        score: 1,
+        items: ["book", "pen"]
+    },
+];
+// Create an array using forEach that contains all the usernames from the gameInfo array,
+// add an exclamation point (ie. “!”) to the end of every username.
+// The new array should look like this :
+// const usernames = ["john!", "becky!", "susy!", "tyson!"]
+
+let usernames = []
+gameInfo.forEach(player => {
+    usernames.push(`${player.username}!`)
+})
+
+console.log(usernames)
+
+// 2. Create an array using forEach that contains the usernames of all players with a score bigger than 5.
+// The new array should look like this :
+
+const winners = []
+gameInfo.forEach(player => {
+    player.score > 5 ? winners.push(player.username) : null
+})
+
+console.log(winners)
+
+// 3. Find and display the total score of the users. (Hint: The total score is 71)
+
+console.log(
+    gameInfo.map(player => player.score).reduce((sum, score) => sum + score)
+)
