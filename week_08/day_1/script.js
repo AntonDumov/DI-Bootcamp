9b6b29aea876346aa259164e1904bbaa96a9890f
Anton Dumov <anton.dumov@gmail.com>
2024-01-14 18:41:03 +0200

    week 8 day 1 exercise

---
+++ week_08/day_1/script.js
@@ -0,0 +1,8 @@
+// const vowels = ['a', 'e', 'i', 'o', 'u']
+//
+// const countVowels = string => {
+//     return string.split('').filter(char => vowels.includes(char)).length
+// }
+//
+// console.log(countVowels('The input string will only consist of lower case letters and'))
+
