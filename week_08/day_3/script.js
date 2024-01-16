54df45d4ad9be4d509c67b3418d7198764988a32
Anton Dumov <anton.dumov@gmail.com>
2024-01-16 20:05:32 +0200

    day 3 exercises

--- week_08/day_3/script.js
+++ week_08/day_3/script.js
@@ -20,13 +20,38 @@
 //
 // console.log(greaterThan3([0,1,1,2,3,5,8]))
 
-const arr = [
-    {id:1 ,name:'John', email:'jjj@gmail.com'},
-    {id:2 ,name:'Mor', email:'mmm@gmail.com'},
-    {id:3 ,name:'Marry', email:'marry@gmail.com'},
-    {id:4 ,name:'Or', email:'or@gmail.com'},
-]
+// const arr = [
+//     {id:1 ,name:'John', email:'jjj@gmail.com'},
+//     {id:2 ,name:'Mor', email:'mmm@gmail.com'},
+//     {id:3 ,name:'Marry', email:'marry@gmail.com'},
+//     {id:4 ,name:'Or', email:'or@gmail.com'},
+// ]
+//
+// console.log(
+//     arr.filter(o => o.name.toLowerCase().includes('o'))
+// )
 
-console.log(
-    arr.filter(o => o.name.toLowerCase().includes('o'))
-)
+let employees = [
+    {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 27,
+        joinedDate: 'December 15, 2017'
+    },
+
+    {
+        firstName: 'Ana',
+        lastName: 'Rosy',
+        age: 25,
+        joinedDate: 'January 15, 2019'
+    },
+
+    {
+        firstName: 'Zion',
+        lastName: 'Albert',
+        age: 30,
+        joinedDate: 'February 15, 2011'
+    }
+];
+
+console.log(employees.filter(e => e.age > 27))
