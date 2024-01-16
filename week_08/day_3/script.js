538d9f7c29e7d6c31ae30a1e0777422945411de6
Anton Dumov <anton.dumov@gmail.com>
2024-01-16 20:45:55 +0200

    day 3 exercises

--- week_08/day_3/script.js
+++ week_08/day_3/script.js
@@ -64,12 +64,39 @@
  * result 117
  */
 
-const sum = arr => {
-    let result = 0
-    for (const element of arr) {
-        result += element
+// const sum = arr => {
+//     let result = 0
+//     for (const element of arr) {
+//         result += element
+//     }
+//     return result
+// }
+//
+// console.log(sum([2, 5, 10,100]))
+
+/**
+ Given n, take the sum of the digits of n.
+ If that value has more than one digit,
+ continue reducing in this way until a single-digit
+ number is produced.
+ This is only applicable to the natural numbers.
+ Examples
+ 16  -->  1 + 6 = 7
+ 942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+ 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+ 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+ */
+
+const reduce2oneDigit = n => {
+    let sumDigits = n.toString().split('').reduce((sum, digit) => {
+        return sum += parseInt(digit)
+    }, 0)
+    if (sumDigits.toString().length > 1) {
+        return reduce2oneDigit(sumDigits)
     }
-    return result
+    else {
+        return sumDigits
+    }
 }
 
-console.log(sum([2, 5, 10,100]))
+console.log(reduce2oneDigit(132189))
