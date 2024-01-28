d78403cb8a5fd6b319edfd11fb7514b13b7f1a83
Anton Dumov <anton.dumov@gmail.com>
2024-01-28 18:51:09 +0200

    week 9 day 4 exercise xp

---
+++ week_09/day_4/exercise_xp/script.js
@@ -0,0 +1,63 @@
+// 🌟 Exercise 1 : HTML Form
+// Instructions
+// Hint : Read about sending form data using the HTTP protocol
+
+// Create a form like the one above. The form should contain three inputs:
+//     a small text input asking for a name,
+//     a larger textarea input to write a message,
+//     a submit input (“Send”)
+
+// When you click the Send button, the form will be submitted with a GET method.
+// (you can use the same HTML file for the action url)
+
+// Where will the sent data appear?
+// a: Data will be sent as GET parameters, we can see them in the address bar,
+// and user can repeat get request without the form, just copying and using resulting url
+
+// 🌟 Exercise 2 : HTML Form #2
+// Instructions
+
+// Use the same form structure as Exercise 1.
+// When you click the Send button, the form will be submitted with a POST method.
+// (you can use the same HTML file for the action url)
+
+// Where will the sent data appear? Hint : Look at the Network Tab
+// a: Data will be sent as POST parameters.
+// user can't repeat post request without filling and submitting a form
+
+// 🌟 Exercise 3 : JSON Mario
+// Instructions
+
+// Using this code:
+
+const marioGame = {
+    detail : "An amazing game!",
+    characters : {
+        mario : {
+            description:"Small and jumpy. Likes princesses.",
+            height: 10,
+            weight: 3,
+            speed: 12,
+        },
+        bowser : {
+            description: "Big and green, Hates princesses.",
+            height: 16,
+            weight: 6,
+            speed: 4,
+        },
+        princessPeach : {
+            description: "Beautiful princess.",
+            height: 12,
+            weight: 2,
+            speed: 2,
+        }
+    },
+}
+
+// Convert this JS object into a JSON object. What happens to the nested objects ?
+marioGameJson = JSON.stringify(marioGame, null, 4)
+
+// Convert and pretty print this JS object into a JSON object. Hint : Check out the JSON lesson on the platform.
+console.log(marioGameJson)
+
+// Use your web inspector to add a breakpoint. Check the values of the JSON object in the debugger.
