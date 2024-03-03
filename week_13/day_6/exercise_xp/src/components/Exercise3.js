709e84137c6ffc7493e4222cfbd9eb226b02a869
AntonDumov <anton.dumov@gmail.com>
2024-03-03 13:41:36 +0200

    week 13 day 6 exercise xp exercise 4 done

--- week_13/day_6/exercise_xp/src/components/Exercise3.js
+++ week_13/day_6/exercise_xp/src/components/Exercise3.js
@@ -1,13 +1,23 @@
 import logo from '../logo.svg'
 import React from 'react'
+import './Exercise.css'
 
 class Exercise extends React.Component {
+    constructor() {
+        super();
+        this.style_header = {
+            color: "white",
+            backgroundColor: "DodgerBlue",
+            padding: "10px",
+            fontFamily: "Arial"
+        };
+    }
     render () {
         return (
             <div>
-                <h1 style={{color:'red', backgroundColor:'lightblue'}}>This is a header</h1>
-                <p>This is a paragraph</p>
-                <a href={'#'}>This is a link</a>
+                <h1 style={this.style_header}>This is a header</h1>
+                <p className={'para'}>This is a paragraph</p>
+                <a href={'/'}>This is a link</a>
                 <img src={logo} alt={''}/>
                 <ul>
                     <li>1</li>
