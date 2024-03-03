26f9da897326d7fcdaa799f2155681adc1bb0186
AntonDumov <anton.dumov@gmail.com>
2024-03-03 20:25:29 +0200

    mini project done

--- week_15/day_1/mini_project/src/Components/RandomQuote.jsx
+++ week_15/day_1/mini_project/src/Components/RandomQuote.jsx
@@ -3,6 +3,36 @@ import quotes from '../QuotesDatabase.js';
 import { useEffect, useState } from 'react';
 
 export const RandomQuote = () => {
+
+    const backgroundColors = [
+        "#FFFFFF",
+        "#CCCCCC",
+        "#666666",
+        "#FF0000",
+        "#00FF00",
+        "#0000FF",
+        "#FFFF00",
+        "#FF00FF",
+        "#00FFFF",
+    ];
+
+    const quoteAndButtonColors = [
+        '#000000',
+        '#333333',
+        '#4169E1',
+        '#DC143C',
+        '#228B22',
+        '#95A5A6',
+        '#795548',
+        '#C02942',
+        '#16A085',
+        '#FFD700'
+    ];
+
+    const getRandomColor = (colorArray) => {
+        return colorArray[Math.floor(Math.random() * colorArray.length)];
+    };
+
     const [quotesPool, setQuotesPool] = useState([...quotes]);
     const [usedQuotes, setUsedQuotesPool] = useState([]);
 
@@ -25,8 +55,22 @@ export const RandomQuote = () => {
         setCurrentQuote(getRandomQuote());
     }, []);
 
+    const [backgroundColor, setBackgroundColor] = useState('#fff');
+    const [quoteColor, setQuoteColor] = useState('#515372');
+    const [buttonColor, setButtonColor] = useState('#515372');
+
+    function setRandomColors() {
+        setBackgroundColor(getRandomColor(backgroundColors));
+        setQuoteColor(getRandomColor(quoteAndButtonColors));
+        setButtonColor(getRandomColor(quoteAndButtonColors));
+    }
+
     return (
-        <div className="random-quote-container">
+        <div className="random-quote-container" style={{
+            '--background-color': backgroundColor,
+            '--quote-color': quoteColor,
+            '--button-color': buttonColor
+        }}>
             <div className="quote">
                 <span>{currentQuote.quote}</span>
             </div>
@@ -34,7 +78,11 @@ export const RandomQuote = () => {
                 <span>{currentQuote.author}</span>
             </div>
             <div className="action">
-                <button onClick={() => setCurrentQuote(getRandomQuote())}>New quote</button>
+                <button onClick={() => {
+                    setCurrentQuote(getRandomQuote());
+                    setRandomColors();
+                }}>New quote
+                </button>
             </div>
         </div>
     );
