23b8bb0eb3de3cc43eb9544b0294b8c891be5436
AntonDumov <anton.dumov@gmail.com>
2024-03-12 19:16:04 +0200

    Update to use Materialize CSS

--- week_15/day_5/mini_project/src/App.js
+++ week_15/day_5/mini_project/src/App.js
@@ -1,22 +1,28 @@
 import { TodoContextProvider } from './TodoContext';
 import DisplayTodos from './Components/DisplayTodos';
 import AddTodo from './Components/AddTodo';
-import './App.css'
+import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import { useEffect } from 'react';
 
 function App() {
-  return (
-      <TodoContextProvider>
-        <div className='container'>
-          <h1>Todo's</h1>
-          <div className='todos-dispay-container'>
-            <DisplayTodos/>
-          </div>
-          <div className='todos-adder-container'>
-            <AddTodo/>
-          </div>
-        </div>
-      </TodoContextProvider>
-  );
+    useEffect(() => {
+        M.AutoInit();
+    }, []);
+    return (
+        <TodoContextProvider>
+            <div className="container">
+                <h1>Todo's</h1>
+                <div className="todos-dispay-container">
+                    <DisplayTodos/>
+                </div>
+                <div className="todos-adder-container">
+                    <AddTodo/>
+                </div>
+            </div>
+        </TodoContextProvider>
+    );
 }
 
 export default App;
