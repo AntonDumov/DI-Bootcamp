af999eb8914a900ef47fb3276e2a890b9e3eff9d
AntonDumov <anton.dumov@gmail.com>
2024-04-16 19:19:29 +0300

    "added exception handling for division by zero in daily_challenge.py"

---
+++ week_20/day_4/daily_challenge.py
@@ -0,0 +1,8 @@
+# Instructions
+# Write a function to compute 5/0 and use try/except to catch the exceptions.
+# Bonus : use some Buit-in exceptions of Python : Look here
+
+try:
+    result = 5/0
+except ZeroDivisionError:
+    print("Cannot divide by zero")
